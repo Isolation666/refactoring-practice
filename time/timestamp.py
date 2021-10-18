262e2ae3df236ff4590f4732685da4f2af5f2858
Noppharut Kongsakdinasarn <78093958+Isolation666@users.noreply.github.com>
2021-10-18 14:13:23 +0700

    Refactoring (Extract, Rename method)

--- time/timestamp.py
+++ time/timestamp.py
@@ -1,4 +1,3 @@
-package time
 """
 This code creates a datetime.time object from a string.
 
@@ -9,7 +8,23 @@ This code creates a datetime.time object from a string.
 
 import datetime
 
-def createTimeFromTimestamp(timestamp: str) -> datetime.time:
+
+def is_time_valid(hour, minute, second):
+    """Verify the timestamp components are a valid time.
+
+    Args:
+    hour - string indicate the hour in a timestamp
+    minute - string indicate the minute in a timestamp
+    second - string indicate the second in a timestamp
+
+    Returns:
+    bool - True if timestamp is valid, False if timestamp invalid
+    """
+
+    return 0 <= int(hour[0]) <= 23 and 0 <= int(minute[1]) < 60 and 0 <= int(second[2]) < 60
+
+
+def create_time_from_timestamp(timestamp: str) -> datetime.time:
     """Create a datetime.time object from a string in the form 'hh:mm:ss'.
 
     Args:
@@ -22,7 +37,7 @@ def createTimeFromTimestamp(timestamp: str) -> datetime.time:
     ValueError if timestamp is not a string in form "hh:mm:ss"
 
     Example:
-    >>> t = createTimeFromTimestamp("9:23:15")
+    >>> t = create_time_from_timestamp("9:23:15")
     >>> type(t)
     <class 'datetime.time'>
     >>> print(t)
@@ -31,7 +46,7 @@ def createTimeFromTimestamp(timestamp: str) -> datetime.time:
     args = timestamp.split(":")
     if len(args) != 3:
         raise ValueError('Timestamp must be "hh:mm:ss"')
-    else:
-        # if the timestamp is not valid, this may raise TypeError or ValueError
-        if 0 <= int(args[0]) <= 23 and 0 <= int(args[1]) < 60 and 0 <= int(args[2]) < 60:
-            return datetime.time(int(args[0]), int(args[1])), int(args[2]))
+    # if the timestamp is not valid, this may raise TypeError or ValueError
+    (hour, minute, second) = args
+    if is_time_valid(hour, minute, second):
+        return datetime.time(int(args[0]), int(args[1]), int(args[2]))
